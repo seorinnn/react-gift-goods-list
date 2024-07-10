31ef50d7b10d8e00bb98a62bc8c3c83447891ed8
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-07-10 15:54:34 +0900

    feat : theme 목록 데이터 받아오는 useTheme hook 추가

---
+++ src/hooks/useThemes.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+import type { ThemeData } from '../types';
+
+interface ThemesResponse {
+  themes: ThemeData[];
+}
+
+export const useThemes = () => {
+  const [themes, setThemes] = useState<ThemeData[]>([]);
+
+  useEffect(() => {
+    const fetchThemes = async () => {
+      try {
+        const response = await axios.get<ThemesResponse>(
+          'https://react-kakao-gift-mock-api.vercel.app/api/v1/themes',
+        );
+        setThemes(response.data.themes);
+      } catch (err) {
+        console.error('API 요청 실패:', err);
+      }
+    };
+
+    fetchThemes();
+  }, []);
+
+  return { themes };
+};
